98eed11030c7f15cf94e4c02f9d01b004646f04c
jaymegardynik <jaymegardynik@gmail.com>
2014-12-20 22:25:35 -0500

    Initial commit, generates mazes and UI setup

---
+++ src/Loader.java
@@ -0,0 +1,33 @@
+
+// TODO Fix this header..
+public class Loader {
+
+	
+	/*
+	 * Example use case:
+	 * 
+	 * Loader myLoader = new Loader();
+	 * myLoader.load("assets\dictionary.txt");
+	 * 
+	 * // Prints out the next random 
+	 * System.out.println(myLoader.nextRandom());
+	 * 
+	 */
+	
+	
+	// TODO Setup an arraylist of Strings (uninitialized)
+	
+	
+	// TODO Make constructor that setups a new arraylist (initialized)
+	
+	
+	// TODO Make load function that returns a boolean on whether or not it loaded properly (use try/catch)
+	// read from the text file, keep it in an assets folder if possible so we can package it in the JAR
+	// every string you read in from the file, add to the array list in this function.
+	// This function should take in the filename to load from (assets/myfile.txt for example?)
+	
+	
+	// TODO Make a nextRandom function that returns a random element of the arraylist
+	
+	
+}
